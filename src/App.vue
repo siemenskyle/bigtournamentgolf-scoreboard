0ffcb1703bd1d227f69f45c209aa858cc1997474
Kyle Siemens <siemenskyle@gmail.com>
2021-01-27 20:57:47 -0500

    fix css, preload images

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,6 @@
 
 <script>
 export default {
-  name: 'app',
-  components: {
-  }
+  name: 'app'
 }
 </script>
-
-<style scoped>
-</style>
--- src/components/MatchScorecard.vue
+++ src/components/MatchScorecard.vue
@@ -5,63 +5,23 @@
         <!-- Player Cards and Up Indicators -->
         <span id="p1Wrapper" class="wrappers">
             <img id="p1Portrait" :src="require(`../assets/${p1Char}`)" class="portrait" />
-            <span id="p1Name" class="names">{{ p1Name }}</span>
+            <span id="p1Name" class="names" v-bind:style="{ fontSize: nameSize(p1Name) }">{{ p1Name }}</span>
 
-            <template v-if="up > 0 && up + lastHole === 17">
-                <img id="p1UpNum" src="../assets/e.png"  class="up" />
-                <img id="p1Up" src="../assets/e.png"  class="up" />
-                <img id="p1Dormie" src="../assets/updormie.png" class="dormie" />
-            </template>
-            <template v-else-if="up > 0 && up + lastHole === 18">
-                <img id="p1UpNum" src="../assets/e.png"  class="up" />
-                <img id="p1Up" src="../assets/e.png"  class="up" />
-                <img id="p1Dormie" src="../assets/dormie.png" class="dormie" />
-            </template>
-            <template v-else-if="up > 0 && up + lastHole > 18">
-                <img id="p1UpNum" src="../assets/e.png"  class="up" />
-                <img id="p1Up" src="../assets/e.png"  class="up" />
-                <img id="p1Dormie" src="../assets/winner.png" class="dormie" />
-            </template>
-            <template v-else-if="up > 0">
-                <img id="p1UpNum" :src="require(`../assets/${up}.png`)" class="up" />
-                <img id="p1Up" src="../assets/UP.png" class="up" />
-                <img id="p1Dormie" src="../assets/e.png" class="dormie" />
-            </template>
-            <template v-else>
-                <img id="p1UpNum" src="../assets/e.png" class="up" />
-                <img id="p1Up" src="../assets/e.png" class="up" />
-                <img id="p1Dormie" src="../assets/e.png" class="dormie" />
+            <template v-for="u in ups(1)" :key="u.up">
+                <img id="p1UpNum" :src="u['upNum']"  class="up" />
+                <img id="p1Up" :src="u['up']"  class="up" />
+                <img id="p1Dormie" :src="u['dormie']" class="dormie" />
             </template>
         </span>
 
         <span id="p2Wrapper" class="wrappers">
             <img id="p2Portrait" :src="require(`../assets/${p2Char}`)" class="portrait" />
-            <span id="p2Name" class="names">{{ p2Name }}</span>
+            <span id="p2Name" class="names" v-bind:style="{ fontSize: nameSize(p2Name) }">{{ p2Name }}</span>
 
-            <template v-if="up < 0 && -1*up + lastHole === 17">
-                <img id="p2UpNum" src="../assets/e.png"  class="up" />
-                <img id="p2Up" src="../assets/e.png"  class="up" />
-                <img id="p2Dormie" src="../assets/updormie.png" class="dormie" />
-            </template>
-            <template v-else-if="up < 0 && -1*up + lastHole === 18">
-                <img id="p2UpNum" src="../assets/e.png"  class="up" />
-                <img id="p2Up" src="../assets/e.png"  class="up" />
-                <img id="p2Dormie" src="../assets/dormie.png" class="dormie" />
-            </template>
-            <template v-else-if="up < 0 && -1*up + lastHole > 18">
-                <img id="p2UpNum" src="../assets/e.png"  class="up" />
-                <img id="p2Up" src="../assets/e.png"  class="up" />
-                <img id="p2Dormie" src="../assets/winner.png" class="dormie" />
-            </template>
-            <template v-else-if="up < 0">
-                <img id="p2UpNum" :src="require(`../assets/${-1 * up}.png`)" class="up" />
-                <img id="p2Up" src="../assets/UP.png" class="up" />
-                <img id="p2Dormie" src="../assets/e.png" class="dormie" />
-            </template>
-            <template v-else>
-                <img id="p2UpNum" src="../assets/e.png" class="up" />
-                <img id="p2Up" src="../assets/e.png" class="up" />
-                <img id="p2Dormie" src="../assets/e.png" class="dormie" />
+            <template v-for="u in ups(2)" :key="u.up">
+                <img id="p2UpNum" :src="u['upNum']"  class="up" />
+                <img id="p2Up" :src="u['up']"  class="up" />
+                <img id="p2Dormie" :src="u['dormie']" class="dormie" />
             </template>
         </span>
 
@@ -101,7 +61,6 @@
             </div>
         </div>
     </div>
-    <div id="scores" class="hide"></div>
   </div>
 </template>
 
@@ -142,12 +101,78 @@ export default {
             ] 
       };
   },
+  methods: {
+      nameSize: function (name) {
+          if (name.length > 14) {
+              return '19px';
+          } else if (name.length > 11) {
+              return '23px';
+          } else {
+              return '29px';
+          }
+      },
+      ups: function(player) {
+          const up = this.up;
+          const lastHole = this.lastHole;
+          const empty = require("../assets/e.png");
+          const upImg = require("../assets/UP.png");
+          const updormie = require("../assets/updormie.png");
+          const dormie = require("../assets/dormie.png");
+          const winner = require("../assets/winner.png");
+          let imgs = {
+              'up': empty,
+              'upNum': empty,
+              'dormie': empty
+          };
+          if (player === 1) {
+              if (up > 0) {
+                  if (up + lastHole === 17) {
+                      imgs['dormie'] = updormie;
+                  } else if (up + lastHole === 18) {
+                      imgs['dormie'] = dormie;
+                  } else if (up + lastHole > 18) { 
+                      imgs['dormie'] = winner;
+                  } else {
+                      imgs['up'] = upImg;
+                      imgs['upNum'] = require(`../assets/${up}.png`);
+                  }
+              } 
+          } else {
+              if (up < 0) {
+                  const p2up = -1 * up;
+                  if (p2up + lastHole === 17) {
+                      imgs['dormie'] = updormie;
+                  } else if (p2up + lastHole === 18) {
+                      imgs['dormie'] = dormie;
+                  } else if (p2up + lastHole > 18) { 
+                      imgs['dormie'] = winner;
+                  } else {
+                      imgs['up'] = upImg;
+                      imgs['upNum'] = require(`../assets/${p2up}.png`);
+                  }
+              } 
+          }
+          return [imgs];
+      }
+  },
   mounted() {
+      // Preload images
+      const imgs = [require("../assets/e.png"), require("../assets/UP.png"), require("../assets/updormie.png"), require("../assets/dormie.png"), require("../assets/winner.png"),
+                    require("../assets/1.png"),require("../assets/2.png"),require("../assets/3.png"),require("../assets/4.png"),require("../assets/5.png"),require("../assets/6.png"),
+                    require("../assets/7.png"),require("../assets/8.png"),require("../assets/9.png"),require("../assets/T.png"),require("../assets/W.png"),require("../assets/L.png"),
+                    require("../assets/PUTTMASTER.gif"),require("../assets/SHOTMAKER.gif"),require("../assets/TECHNITIAN.gif"),require("../assets/POWERGOLFER.gif"),require("../assets/VETERAN.gif"),
+                    require("../assets/YOUNGHERO.gif")];
+      imgs.forEach( src => {
+          let img = new Image();
+          img.onload = () => {
+            console.log('img preloaded');
+          }
+          img.src = src;
+      });
+      // Get Match & Watch DB
       const itemRef = fb.database().ref(`matches/${this.matchID}`);
-      console.log(itemRef)
       itemRef.on("value", snapshot => {
           let data = snapshot.val();
-          console.log(data)
           this.p1Name = data['p1Name'];
           this.p2Name = data['p2Name'];
           this.p1Char = data['p1Char'];
@@ -237,7 +262,8 @@ export default {
 
 .dormie {
     position: relative;
-    margin-top: -86px;
+    margin-top: -84px;
+    margin-bottom: 28px;
     margin-left: 14px;
     width: 262px;
     z-index: -1;
@@ -262,150 +288,147 @@ export default {
     position: relative;
 }
 #h1p1 {
-    left: 99px;
-    top: -249px;
+    left: 111px;
+    top: -253px;
 }
 #h1p2 {
-    left: 56px;
-    top: -202px;
+    left: 73px;
+    top: -205px;
 }
 #h2p1 {
-    left: 64px;
-    top: -249px;
+    left: 84px;
+    top: -253px;
 }
 #h2p2 {
-    left: 21px;
-    top: -202px;
+    left: 47px;
+    top: -205px;
 }
 #h3p1 {
-    left: 28px;
-    top: -249px;
+    left: 56px;
+    top: -253px;
 }
 #h3p2 {
-    left: -15px;
-    top: -202px;
+    left: 19px;
+    top: -205px;
 }
 #h4p1 {
-    left: -9px;
-    top: -249px;
+    left: 29px;
+    top: -253px;
 }
 #h4p2 {
-    left: -52px;
-    top: -202px;
+    left: -9px;
+    top: -205px;
 }
 #h5p1 {
-    left: -45px;
-    top: -249px;
+    left: 1px;
+    top: -253px;
 }
 #h5p2 {
-    left: -88px;
-    top: -202px;
+    left: -37px;
+    top: -205px;
 }
 #h6p1 {
-    left: -82px;
-    top: -249px;
+    left: -26px;
+    top: -253px;
 }
 #h6p2 {
-    left: -125px;
-    top: -202px;
+    left: -64px;
+    top: -205px;
 }
 #h7p1 {
-    left: -117px;
-    top: -249px;
+    left: -53px;
+    top: -253px;
 }
 #h7p2 {
-    left: 392px;
-    top: -245px;
+    left: -91px;
+    top: -205px;
 }
 #h8p1 {
-    left: 397px;
-    top: -293px;
+    left: -80px;
+    top: -253px;
 }
 #h8p2 {
-    left: 355px;
-    top: -245px;
+    left: 453px;
+    top: -247px;
 }
 #h9p1 {
-    left: 362px;
-    top: -293px;
+    left: 463px;
+    top: -295px;
 }
 #h9p2 {
-    left: 320px;
-    top: -245px;
+    left: 425px;
+    top: -247px;
 }
 #h10p1 {
-    left: -112px;
-    top: -153px;
+    left: -2px;
+    top: -156px;
 }
 #h10p2 {
-    left: -155px;
-    top: -106px;
+    left: -40px;
+    top: -108px;
 }
 #h11p1 {
-    left: -149px;
-    top: -153px;
+    left: -30px;
+    top: -156px;
 }
 #h11p2 {
-    left: -192px;
-    top: -106px;
+    left: -68px;
+    top: -108px;
 }
 #h12p1 {
-    left: -184px;
-    top: -153px;
+    left: -57px;
+    top: -156px;
 }
 #h12p2 {
-    left: -227px;
-    top: -106px;
+    left: -95px;
+    top: -108px;
 }
 #h13p1 {
-    left: -221px;
-    top: -153px;
+    left: -85px;
+    top: -156px;
 }
 #h13p2 {
-    left: -263px;
-    top: -106px;
+    left: -122px;
+    top: -108px;
 }
 #h14p1 {
-    left: 230px;
-    top: -196px;
+    left: -112px;
+    top: -156px;
 }
 #h14p2 {
-    left: 188px;
-    top: -148px;
+    left: -148px;
+    top: -108px;
 }
 #h15p1 {
-    left: 195px;
-    top: -196px;
+    left: -139px;
+    top: -156px;
 }
 #h15p2 {
-    left: 153px;
-    top: -148px;
+    left: -177px;
+    top: -108px;
 }
 #h16p1 {
-    left: 159px;
-    top: -196px;
+    left: 232px;
+    top: -198px;
 }
 #h16p2 {
-    left: 117px;
-    top: -148px;
+    left: 195px;
+    top: -150px;
 }
 #h17p1 {
-    left: 123px;
-    top: -196px;
+    left: 206px;
+    top: -198px;
 }
 #h17p2 {
-    left: 80px;
-    top: -148px;
+    left: 168px;
+    top: -150px;
 }
 #h18p1 {
-    left: 87px;
-    top: -196px;
+    left: 178px;
+    top: -198px;
 }
 #h18p2 {
-    left: 45px;
-    top: -148px;
-}
-.hide {
-    display: none;
+    left: 140px;
+    top: -150px;
 }
 </style>
